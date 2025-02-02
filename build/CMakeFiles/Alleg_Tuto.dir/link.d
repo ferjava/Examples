47f402798682e18559e807772e765d9dc7b7472e
ferjava <fgsj.fg@gmail.com>
2025-02-02 21:32:34 +0100

    Inicio Poryecto

---
+++ build/CMakeFiles/Alleg_Tuto.dir/link.d
@@ -0,0 +1,157 @@
+Alleg_Tuto: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/Alleg_Tuto.dir/main.cpp.o \
+  /home/ferjava/WorkSpace/MisJuegos/TutoAlle/lib_x86_64/lib/liballegro.so \
+  /home/ferjava/WorkSpace/MisJuegos/TutoAlle/lib_x86_64/lib/liballegro_font.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o \
+  /usr/lib/libSM.so.6 \
+  /usr/lib/libICE.so.6 \
+  /usr/lib/libX11.so.6 \
+  /usr/lib/libXext.so.6 \
+  /usr/lib/libXcursor.so.1 \
+  /usr/lib/libXi.so.6 \
+  /usr/lib/libXinerama.so.1 \
+  /usr/lib/libXrandr.so.2 \
+  /usr/lib/libXss.so.1 \
+  /usr/lib/libOpenGL.so.0 \
+  /usr/lib/libGLU.so.1 \
+  /usr/lib/libGLX.so.0 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/libuuid.so.1 \
+  /usr/lib/libxcb.so.1 \
+  /usr/lib/libXrender.so.1 \
+  /usr/lib/libXfixes.so.3 \
+  /usr/lib/libGLdispatch.so.0 \
+  /usr/lib/libXau.so.6 \
+  /usr/lib/libXdmcp.so.6
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/Alleg_Tuto.dir/main.cpp.o:
+
+/home/ferjava/WorkSpace/MisJuegos/TutoAlle/lib_x86_64/lib/liballegro.so:
+
+/home/ferjava/WorkSpace/MisJuegos/TutoAlle/lib_x86_64/lib/liballegro_font.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o:
+
+/usr/lib/libSM.so.6:
+
+/usr/lib/libICE.so.6:
+
+/usr/lib/libX11.so.6:
+
+/usr/lib/libXext.so.6:
+
+/usr/lib/libXcursor.so.1:
+
+/usr/lib/libXi.so.6:
+
+/usr/lib/libXinerama.so.1:
+
+/usr/lib/libXrandr.so.2:
+
+/usr/lib/libXss.so.1:
+
+/usr/lib/libOpenGL.so.0:
+
+/usr/lib/libGLU.so.1:
+
+/usr/lib/libGLX.so.0:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/libuuid.so.1:
+
+/usr/lib/libxcb.so.1:
+
+/usr/lib/libXrender.so.1:
+
+/usr/lib/libXfixes.so.3:
+
+/usr/lib/libGLdispatch.so.0:
+
+/usr/lib/libXau.so.6:
+
+/usr/lib/libXdmcp.so.6:
---
+++ lib_x86_64/lib/cmake/allegro/AllegroTargets-relwithdebinfo.cmake
@@ -0,0 +1,129 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Allegro::allegro" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro "${_IMPORT_PREFIX}/lib/liballegro-static.a" )
+
+# Import target "Allegro::allegro_primitives" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_primitives APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_primitives PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C;CXX"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_primitives-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_primitives )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_primitives "${_IMPORT_PREFIX}/lib/liballegro_primitives-static.a" )
+
+# Import target "Allegro::allegro_image" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_image APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_image PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_image-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_image )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_image "${_IMPORT_PREFIX}/lib/liballegro_image-static.a" )
+
+# Import target "Allegro::allegro_font" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_font APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_font PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_font-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_font )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_font "${_IMPORT_PREFIX}/lib/liballegro_font-static.a" )
+
+# Import target "Allegro::allegro_audio" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_audio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_audio PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_audio-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_audio )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_audio "${_IMPORT_PREFIX}/lib/liballegro_audio-static.a" )
+
+# Import target "Allegro::allegro_acodec" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_acodec APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_acodec PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_acodec-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_acodec )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_acodec "${_IMPORT_PREFIX}/lib/liballegro_acodec-static.a" )
+
+# Import target "Allegro::allegro_ttf" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_ttf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_ttf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_ttf-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_ttf )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_ttf "${_IMPORT_PREFIX}/lib/liballegro_ttf-static.a" )
+
+# Import target "Allegro::allegro_color" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_color APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_color PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_color-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_color )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_color "${_IMPORT_PREFIX}/lib/liballegro_color-static.a" )
+
+# Import target "Allegro::allegro_memfile" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_memfile APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_memfile PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_memfile-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_memfile )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_memfile "${_IMPORT_PREFIX}/lib/liballegro_memfile-static.a" )
+
+# Import target "Allegro::allegro_dialog" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_dialog APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_dialog PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_dialog-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_dialog )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_dialog "${_IMPORT_PREFIX}/lib/liballegro_dialog-static.a" )
+
+# Import target "Allegro::allegro_video" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_video APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_video PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_video-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_video )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_video "${_IMPORT_PREFIX}/lib/liballegro_video-static.a" )
+
+# Import target "Allegro::allegro_main" for configuration "RelWithDebInfo"
+set_property(TARGET Allegro::allegro_main APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Allegro::allegro_main PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liballegro_main-static.a"
+  )
+
+list(APPEND _cmake_import_check_targets Allegro::allegro_main )
+list(APPEND _cmake_import_check_files_for_Allegro::allegro_main "${_IMPORT_PREFIX}/lib/liballegro_main-static.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
