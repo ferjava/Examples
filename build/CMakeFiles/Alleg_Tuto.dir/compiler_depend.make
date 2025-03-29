ed65a20573ce426814bd4ec5514564560be3024c
ferjava <fgsj.fg@gmail.com>
2025-03-29 08:22:48 +0100

    hola

---
+++ build/CMakeFiles/Alleg_Tuto.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Alleg_Tuto.
+# This may be replaced when dependencies are built.
---
+++ build/engine/CMakeFiles/Fjengine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fjengine.dir/src/Rectangle.cpp.obj"
+  "CMakeFiles/Fjengine.dir/src/Rectangle.cpp.obj.d"
+  "CMakeFiles/Fjengine.dir/src/base/Actor.cpp.obj"
+  "CMakeFiles/Fjengine.dir/src/base/Actor.cpp.obj.d"
+  "CMakeFiles/Fjengine.dir/src/collision/CollisionManager.cpp.obj"
+  "CMakeFiles/Fjengine.dir/src/collision/CollisionManager.cpp.obj.d"
+  "CMakeFiles/Fjengine.dir/src/utils/Vec2.cpp.obj"
+  "CMakeFiles/Fjengine.dir/src/utils/Vec2.cpp.obj.d"
+  "libFjengine.a"
+  "libFjengine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fjengine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
